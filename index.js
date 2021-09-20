3459aceae5ddf5802a77696b638b9b8b367fa09b
Frenzy Singapore <75056113+FrenzY8@users.noreply.github.com>
2021-09-20 05:28:28 +0000

    Updated to Console
    just adding a small updates, but thats very purpose maybe,

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const { FILTER_LIST, BOT_TOKEN } = require('./config.json');
 
-client.login(config.BOT_TOKEN).catch(console.error);
+client.login(config.BOT_TOKEN).catch(console.log("Invalid token!"));
 client.on("ready", () => {
   console.log(`Iam Ready => via github.com/FrenzY8`);
 console.log(`Logged in as ${client.user.tag}`)
@@ -24,6 +24,7 @@ ${FILTER_LIST}
 client.on('message', async message => {
   if(config.FILTER_LIST.some(word => message.content.toLowerCase().includes(word))){
     message.delete()
+    console.log(`>> Message from ${message.author.tag} in ${message.guild.name} has been deleted cause included wrong words`);
 // if you want the bot sending message after delete message =>
     // message.channel.send("i have deleted your messagess cause included Badwords in my ```config.json```");
     
