3eb93f709e9d6b73c57ddc2342100870aa658b33
Frenzy Singapore <75056113+FrenzY8@users.noreply.github.com>
2021-09-20 00:31:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const config = require("./config.json");
 
 
 client.login(config.BOT_TOKEN).catch(console.error);
-client.on('ready', () => {
-  console.log('Now Online, ready to filter!');
+client.on("ready", () => {
+  console.log(`Iam Ready => via github.com/FrenzY8`);
+  client.user.setActivity(`Words Controller via github.com/FrenzY8`);
 });
 
 
 client.on('message', message => {
   if(config.FILTER_LIST.some(word => message.content.toLowerCase().includes(word))){
     message.delete()
+    // if you want the bot sending message after delete message =>
+    // message.channel.send("i have deleted your messagess cause included Badwords in my ```config.json```");
+    
+    // or you want to warn the user via dm =>
+    // message.author.send("im beggins to you, please dont toxic anymores!")
   }})
