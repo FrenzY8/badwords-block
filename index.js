aef0e8d2a795b1d2e26adac2ea88b0134e22889c
Frenzy Singapore <75056113+FrenzY8@users.noreply.github.com>
2021-09-20 00:42:49 +0000

    v1

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ client.on("ready", () => {
 client.on('message', message => {
   if(config.FILTER_LIST.some(word => message.content.toLowerCase().includes(word))){
     message.delete()
-    // if you want the bot sending message after delete message =>
+// if you want the bot sending message after delete message =>
     // message.channel.send("i have deleted your messagess cause included Badwords in my ```config.json```");
     
-    // or you want to warn the user via dm =>
+// or you want to warn the user via dm =>
     // message.author.send("im beggins to you, please dont toxic anymores!")
   }})
