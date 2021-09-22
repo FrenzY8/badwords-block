200a9e9c7e5c2d97064c1f20f4d151c7eb65c6ca
Frenzy Singapore <75056113+FrenzY8@users.noreply.github.com>
2021-09-22 03:19:20 +0000

    just posted //

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+// LOADED THE CONFIGS
 const config = require("./config.json");
 const { FILTER_LIST, BOT_TOKEN } = require('./config.json');
-
+// logged in to the bot
 client.login(config.BOT_TOKEN).catch(console.log("Invalid token!"));
+// succesfully, if you wanted use env, of course you can!
 client.on("ready", () => {
   console.log(`Iam Ready => via github.com/FrenzY8`);
 console.log(`Logged in as ${client.user.tag}`)
 });
-
+// first command, wait, i told you, this not a command bot, just a delete messages
 client.on('message', message => {
-  if(message.content.startsWith('!help')) {
+  if(message.content.startsWith('!help')) { // wil sending you a filter list / words that blocked in your server
     message.author.send(`
     
 My List Words :
@@ -20,10 +22,10 @@ ${FILTER_LIST}
   }
 
 })
-
+// this gay, if any command detected with filter list, bye auto die, jk lol. thats message will be deleted
 client.on('message', async message => {
   if(config.FILTER_LIST.some(word => message.content.toLowerCase().includes(word))){
-    message.delete()
+    message.delete() // delete the message ( DISCORD JS V11 )
     console.log(`>> Message from ${message.author.tag} in ${message.guild.name} has been deleted cause included wrong words`);
 // if you want the bot sending message after delete message =>
     // message.channel.send("i have deleted your messagess cause included Badwords in my ```config.json```");
